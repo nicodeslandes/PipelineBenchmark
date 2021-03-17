40a45cc6600a037c0818333c4dbe63638fbaf733
Nicolas Deslandes <ndeslandes@live.fr>
2021-03-17 00:37:59 +0000

    Implement custom skipping

--- PipelineBenchmark.cpp
+++ PipelineBenchmark.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <chrono>
+#include <random>
+#include <limits>
 
 using namespace std;
 using namespace std::chrono;
@@ -11,42 +13,64 @@ using namespace std::chrono;
 extern "C" int64_t calc(int64_t * arr, size_t size);
 extern "C" int64_t calc_no_jump(int64_t * arr, size_t size);
 extern "C" int64_t calc_no_jump_long_fetches(int64_t * arr, size_t size);
+extern "C" int64_t calc_no_jump_longer_fetches(int64_t * arr, size_t size);
 extern "C" int64_t calc_no_jump_not_so_long_fetches(int64_t * arr, size_t size);
+extern "C" int64_t calc_no_jump_custom_fetches(int64_t * arr, size_t size, size_t skip);
+
+#define SHORT_OUTPUT
 
 int main()
 {
 	cout << "Allocating 1GB longs" << endl;
-	const size_t MemSize = 1024*1024*1024;
+	const size_t MemSize = 1LL*1024LL*1024LL*1024LL;
 	auto size = MemSize / sizeof(int64_t);
 	auto arr = new int64_t[size];
 
+#ifndef SHORT_OUTPUT
 	std::cout.imbue(std::locale(""));
+#endif
 	cout << "Populating the array with random values" << endl;
-	srand((uint32_t)time(nullptr));
+	std::random_device rd;     //Get a random seed from the OS entropy device, or whatever
+	std::mt19937_64 eng(rd()); //Use the 64-bit Mersenne Twister 19937 generator
+							   //and seed it with entropy.
+	//std::ranlux48_base // faster?
+
+	//Define the distribution, by default it goes from 0 to MAX(unsigned long long)
+	//or what have you.
+	std::uniform_int_distribution<int64_t> distr;
+	//std::subtract_with_carry<unsigned long long> distr;
+
+	auto start = high_resolution_clock::now();
 	for (int i = 0; i < size; i++) {
-		arr[i] =
-			((int64_t)rand()) << 0 |
-			((int64_t)rand()) << 15 |
-			((int64_t)rand()) << 30 |
-			((int64_t)rand()) << 45 |
-			((int64_t)rand()) << 60;
+		arr[i] = distr(eng) * ((distr(eng) % 2) ? -1 : 1);
+			//((int64_t)rand()) << 0 |
+			//((int64_t)rand()) << 15 |
+			//((int64_t)rand()) << 30 |
+			//((int64_t)rand()) << 45 |
+			//((int64_t)rand()) << 60;
 		//arr[i] = rand() + ((int64_t)rand()) << 45;
 		if ((i + 1) % (size / 10) == 0) cout << "." << flush;
 		//arr[i] = ((int64_t)i + 1) << (i * 4);
 	}
-
-	cout << endl << "Summing all positive elements" << endl;
-	auto start = high_resolution_clock::now();
-	int64_t sum = 0;
-	for (int i = 0; i < size; i++) {
-		if (arr[i] > 0)
-			sum += arr[i];
-	}
 	auto elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	cout << endl << "Done in " << elapsed.count() << " ms" << endl;
+
+	cout << "Summing all positive elements" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		start = high_resolution_clock::now();
+		int64_t sum = 0;
+		for (int i = 0; i < size; i++) {
+			if (arr[i] > 0)
+				sum += arr[i];
+		}
+
+		elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+		cout << "Result: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	}
 
 	start = high_resolution_clock::now();
-	sum = calc(arr, size);
+	auto sum = calc(arr, size);
 	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
 	cout << "Result for calc: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
@@ -65,40 +89,54 @@ int main()
 	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
 	cout << "Result for calc_no_jump: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	//start = high_resolution_clock::now();
+	//sum = calc_no_jump(arr, size);
+	//elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+	//cout << "Result for calc_no_jump: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+
+	//start = high_resolution_clock::now();
+	//sum = calc_no_jump_long_fetches(arr, size);
+	//elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+	//cout << "Result for calc_no_jump_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
 	start = high_resolution_clock::now();
 	sum = calc_no_jump_long_fetches(arr, size);
 	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
 	cout << "Result for calc_no_jump_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump_long_fetches(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	//start = high_resolution_clock::now();
+	//sum = calc_no_jump_longer_fetches(arr, size);
+	//elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+	//cout << "Result for calc_no_jump_longer_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump_long_fetches(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump_not_so_long_fetches(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump_not_so_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	//start = high_resolution_clock::now();
+	//sum = calc_no_jump_not_so_long_fetches(arr, size);
+	//elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+	//cout << "Result for calc_no_jump_not_so_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump_not_so_long_fetches(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump_not_so_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	//start = high_resolution_clock::now();
+	//sum = calc_no_jump_not_so_long_fetches(arr, size);
+	//elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+	//cout << "Result for calc_no_jump_not_so_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
 
-	start = high_resolution_clock::now();
-	sum = calc_no_jump_not_so_long_fetches(arr, size);
-	elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
-	cout << "Result for calc_no_jump_not_so_long_fetches: " << sum << " (in " << elapsed.count() << " ms)" << endl;
+	auto run_custom_skip = [&](auto skip) {
+		start = high_resolution_clock::now();
+		sum = calc_no_jump_custom_fetches(arr, size, skip);
+		elapsed = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+#ifdef SHORT_OUTPUT
+		cout << skip << " " << elapsed.count() << " " << sum << endl;
+#else
+		cout << "Result for calc_no_jump_custom_fetches(" << skip << "): " << sum << " (in " << elapsed.count() << " ms)" << endl;
+#endif
+		
+	};
+
+	for (int skip = 1; skip <= 65536; skip <<= 1) {
+		run_custom_skip(skip);
+		run_custom_skip(skip);
+		run_custom_skip(skip);
+	}
 
 	delete[] arr;
 	return 0;
--- calc.asm
+++ calc.asm
@@ -107,4 +107,71 @@ exit:
    ret
 calc_no_jump_not_so_long_fetches endp
 
+calc_no_jump_longer_fetches proc
+	; calc(int64_t* arr, size_t count)
+	; rcx is arr, rdx is count
+	mov r11,rcx		; r11 <- arr
+	mov r10, rdx	; r10 <- count
+	test r10,r10	; if count <= 0
+	jle exit		;   return
+	push rsi
+	xor rax,rax		; rax: sum; sum = 0
+	shr r10, 13		; r10 /= 8192
+	mov rdx, 8191	; rdx: index = 8191
+outer:
+	mov rcx, r10	; init rcx to count/8191
+	lea rsi, [r11+rdx*8]	; and rsi to arr + index
+add_start:
+	xor r9,r9
+	mov r8, [rsi]	; r8 <- *rsi
+	test r8,r8		; if r8 > 0,
+	cmovg r9, r8	;    r9 <- r8
+	add rax,r9		; sum += r9
+next:
+	lea rsi, 65536[rsi]	; rsi += 8192*8
+	loop add_start
+	dec rdx				; index--
+	jge outer			; if index>=0, start the loop again
+	pop rsi
+exit:
+   ret
+calc_no_jump_longer_fetches endp
+
+
+calc_no_jump_custom_fetches proc
+	; calc(int64_t* arr, size_t count, size_t skip)
+	; rcx is arr, rdx is count, r8 is skip
+	mov r11,rcx		; r11 <- arr
+	mov r10, rdx	; r10 <- count
+	test r10,r10	; if count <= 0
+	jle exit		;   return
+	push rsi
+	push r12
+	mov rax, r10	; r10 /= skip
+	xor rdx, rdx
+	idiv r8
+	mov r10, rax
+	xor rax,rax		; rax: sum; sum = 0
+	mov rdx, r8		; rdx: index = skip - 1
+	dec rdx
+outer:
+	mov rcx, r10	; init rcx to count/skip
+	lea rsi, [r11+rdx*8]	; and rsi to arr + index
+add_start:
+	xor r9,r9
+	mov r12, [rsi]	; r12 <- *rsi
+	test r12,r12	; if r12 > 0,
+	cmovg r9, r12	;    r9 <- r12
+	add rax,r9		; sum += r9
+next:
+	lea rsi, [rsi + r8*8]	; rsi += skip*8
+	loop add_start
+	dec rdx				; index--
+	jge outer			; if index>=0, start the loop again
+	pop r12
+	pop rsi
+exit:
+   ret
+calc_no_jump_custom_fetches endp
+
 end
